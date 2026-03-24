03d40a75e27986c49023afc46d891f973b6d2317
tjs23 <tjs23@cam.ac.uk>
2026-03-24 11:54:19 +0000

    TJS: Tweak for latest datasets

--- collate_outputs.py
+++ collate_outputs.py
@@ -2,16 +2,15 @@ from glob import glob
 import os
 from schisome import SchisomeDataSet
 
-run_tag = 'Aug25v1'
+run_tag = 'Feb26v1'
 data_paths = glob(f'datasets/*_{run_tag}.npz')
 
 ## ## Add Miguel's experimental lists
 
 for data_path in data_paths:
-   data_set = SchisomeDataSet(data_path)
+   data_set = SchisomeDataSet(data_path, aux_marker_key='suborganelle')
    data_set.info(f'Finalising {data_path}')  
-   data_set.reset_2d_proj()
-   plot_args = dict(min_nonzero=0.9, spot_size=16)
+   #data_set.reset_2d_proj()
    
    if not data_set.has_predictions:
        data_set.warn(f'Dataset at {data_path} has no mixed class and reconstruction predictions. The DNN workflow should be run first.')
@@ -27,16 +26,17 @@ for data_path in data_paths:
            
    if not data_set.has_profile_key('zfill'):
        data_set.info(f'Reconstructing profiles for {data_path}')
-       data_set.make_profile_predictions(max_missing=0.35)
+       data_set.make_profile_predictions(max_missing=0.5)
 
-   data_set.plot_umap_2d('zfill', ['training', 'prediction', 'prediction2'], ['Train', 'Pred', 'Duals'], **plot_args)
-   data_set.plot_umap_2d('recon', ['training', 'prediction', 'prediction2'], ['Train', 'Pred', 'Duals'], **plot_args)
+   plot_args = dict(min_nonzero=0.5, spot_size=16)
+   #data_set.plot_umap_2d('zfill', ['training', 'prediction', 'prediction2'], ['Train', 'Pred', 'Duals'], **plot_args)
+   #data_set.plot_umap_2d('recon', ['training', 'prediction', 'prediction2'], ['Train', 'Pred', 'Duals'], **plot_args)
 
    file_root = os.path.splitext(data_path)[0]
    sqlite_path = file_root + '.sqlite' 
    data_set.info(f'Making SQLite3 database {sqlite_path}')  
-   data_set.write_database(sqlite_path)
+   data_set.write_database(sqlite_path, max_missing=0.5)
 
    profile_tsv_path = file_root + '_profiles.tsv'    
    data_set.write_profile_tsv(profile_tsv_path, profiles=['init','zfill','latent'],
-                              markers=['training','prediction','prediction2'])
+                              markers=['training','prediction','suborganelle'])
